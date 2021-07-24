27fd9428af90f594dd978035c6dd9befda700a37
kategrytsenko <grytsenko.kate.ua@gmail.com>
2021-07-25 00:10:12 +0300

    EFC-4: got tasks from storage

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { DataproviderService } from './dataprovider.service';
 import Student from './model/student';
-import Task from './model/task';
+import { TasksService } from './services/tasks.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   public title = 'Student Tasks';
-  public data: Student[];
+  public data: Student[] = [];
+  public dataFromStorage: Student[] = [];
 
- constructor(dataProvider: DataproviderService){
-   this.data = dataProvider.makeStudentData()
- }
+  constructor (private dataProvider: DataproviderService, private tasksService: TasksService) {
+  }
+
+  ngOnInit (): void {
+    this.dataFromStorage = this.tasksService.getDataFromStorage();
+    this.data = this.dataFromStorage.length ? this.dataFromStorage : this.dataProvider.makeStudentData();
+
+    if (!this.dataFromStorage.length) {
+      this.tasksService.setDataToStorage(this.data);
+    }
+  }
 }
--- src/app/services/tasks.service.ts
+++ src/app/services/tasks.service.ts
@@ -1,14 +1,32 @@
 import { Injectable } from '@angular/core';
 import Student from '../model/student';
+import { find } from 'lodash';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TasksService {
 
-  constructor() { }
+  constructor () {
+  }
+
+  setDataToStorage (data: Student[]) {
+    localStorage.setItem('data', JSON.stringify(data));
+  }
+
+  getDataFromStorage () {
+    const data = localStorage.getItem('data');
+
+    return data ? JSON.parse(data) : [];
+  }
 
   storeTickedTasks (student: Student) {
-    localStorage.setItem(`${student.name} completedTasks:`, JSON.stringify(student.completedTasks));
+    const parsedData = this.getDataFromStorage();
+    const updatedStudent = find(parsedData, studentInStorage => studentInStorage.name === student.name);
+
+    updatedStudent.completedTasks = student.completedTasks;
+    updatedStudent.incompleteTasks = student.incompleteTasks;
+
+    localStorage.setItem('data', JSON.stringify(parsedData));
   }
 }
